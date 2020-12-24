0077f9890d223e289118642cdbd37c31dc94fc22
wan-h <13658247573@163.com>
2020-12-24 20:59:15 +0800

    update prepareData.py

--- scripts/prepareData.py
+++ scripts/prepareData.py
@@ -5,6 +5,9 @@ import os
 import cv2
 from tqdm import tqdm
 
+# 使用1080P的画质
+TARGET_SIZE = (1920, 1080)
+
 def save_frames_from_video(video, output, interval):
     videoCapture = cv2.VideoCapture(video)
     fps = int(videoCapture.get(cv2.CAP_PROP_FPS))
@@ -17,9 +20,11 @@ def save_frames_from_video(video, output, interval):
             count += 1
         if count % interval_frames == 0:
             frame_name = "{}.jpg".format(count)
+            frame = cv2.resize(frame, TARGET_SIZE)
             cv2.imwrite(os.path.join(output, frame_name), frame)
 
 if __name__ == '__main__':
+    # 视频转图像
     save_frames_from_video(
         video='/data/datasets/animegan/your_name.mkv',
         output='/data/datasets/animegan/you_name_more',
